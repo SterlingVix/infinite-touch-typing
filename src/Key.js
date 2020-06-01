e216be5c5babfe78e4a3f2d45cd666189f274b79
Aaron Melocik <amelocik@gmail.com>
2020-06-01 13:05:51 -0700

    Style keys.

--- src/Key.js
+++ src/Key.js
@@ -9,27 +9,49 @@ export default class Key extends Component {
     onKeyClick: PropTypes.func.isRequired
   };
 
+  constructor(props) {
+    super(props);
+  }
+
+  getKeyBackground = isPressed =>
+    `background: ${isPressed ? "lightskyblue" : "lavenderblush"};`;
+  getKeyBoxShadow = isPressed =>
+    `box-shadow: ${
+      isPressed ? `3px 3px 5px` : `7px 7px 7px`
+    } 0px rgba(0, 0, 0, 0.66);`;
+
+  setKeyColor = isPressed => `
+    background: ${isPressed ? "lightskyblue" : "lavenderblush"};
+    box-shadow: ${
+      isPressed ? `3px 3px 5px` : `7px 7px 7px`
+    } 0px rgba(0, 0, 0, 0.66);
+  `;
+
   handleKeyClick = event => {
     const { keyVal, onKeyClick } = this.props;
     console.log(event);
     onKeyClick(keyVal);
   };
 
-  constructor(props) {
-    super(props);
-  }
-
   render = () => {
     const { isInPractice, keyVal } = this.props;
 
     const KeyChar = styled.div`
-      border: solid 1px black;
+      // border: solid 1px black;
+      ${this.setKeyColor(isInPractice)}
       border-radius: 0.33em;
-      margin: 0.1em;
-      padding: 1em;
+      color: black;
       cursor: pointer;
+      font-weight: 700;
+      margin: 0 0.2em;
+      padding: 1em;
       user-select: none;
-      background-color: ${isInPractice ? "green" : "white"};
+      width: 1em;
+
+      &:hover {
+        ${this.setKeyColor(!isInPractice)}
+        opacity: 0.5;
+      }
     `;
 
     return <KeyChar onClick={this.handleKeyClick}>{keyVal}</KeyChar>;
